8f9547bccb0cecf06d8edae7b346a3c8a8e100f1
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-10-20 23:47:00 +0200

    feat: adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+            black: {
+                DEFAULT: "#000",
+                100: "#00039"
+            }
+        }
+      },
+    },
+    plugins: [],
+  }
